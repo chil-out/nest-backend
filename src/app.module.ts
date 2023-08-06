ffaedd8507e178089bd340a443cfe37349129aa8
rocket-x <zcjb.rjdj@gmail.com>
2023-08-07 02:33:44 +0800

    init typeorm and create db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { Permission } from './user/entities/permission.entity';
+import { Role } from './user/entities/role.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'demo',
+      synchronize: true,
+      logging: true,
+      entities: [User, Role, Permission],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        autoPlugin: 'sha256-password',
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/entities/permission.entity.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity({
+  name: 'permissions',
+})
+export class Permission {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 20,
+    comment: '权限代码',
+  })
+  code: string;
+
+  @Column({
+    length: 100,
+    comment: '权限描述',
+  })
+  description: string;
+}
